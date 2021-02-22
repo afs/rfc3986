8e94ac4481fd9746bfa0096b03dded1128b527c1
Andy Seaborne <andy@seaborne.org>
2021-02-22 09:44:08 +0000

    Split parser detailed tests

--- src/test/java/org/seaborne/rfc3986/TS_RFC3986.java
+++ src/test/java/org/seaborne/rfc3986/TS_RFC3986.java
@@ -23,6 +23,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( {
+    TestParseComponents.class,
     TestRFC3986.class,
     TestRFC3986_Features.class,
     TestResolve.class,
